ed11d28e854f9d11b0de22040929101f790474fb
Jerry Plotke <jerry.plotke@gmail.com>
2021-09-07 13:15:37 -0500

    query and mutation types defined

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,18 @@
 const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+  }
+
+  type Mutation {
+    login(email: String, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: SaveBookInput!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
 module.exports = typeDefs;
