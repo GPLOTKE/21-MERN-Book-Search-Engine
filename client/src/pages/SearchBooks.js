ccf6673408bfad3ec846c9b5f5415ef0bb801a22
Jerry Plotke <jerry.plotke@gmail.com>
2021-09-14 15:39:21 -0500

    use mutation hook to execute save book mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -17,6 +17,15 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  const [saveBook] = useMutation(SAVE_BOOK,{ update(cache, { data: { saveBook }}) {
+    
+    const { me } = cache.readQuery({ query: GET_ME });
+    cache.writeQuery({
+      query: GET_ME,
+      data: { me: { ...me, savedBooks: [...me.savedBooks, saveBook] } },
+    });
+  } 
+})
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
